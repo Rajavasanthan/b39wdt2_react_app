7391738bb263b81944458d603d397b2f3bade6bf
rajavasanthan <rajavasanthan@gmail.com>
2022-11-10 11:08:59 +0530

    header added

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
-import { useFormik } from 'formik';
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { config } from './config';
+import axios from "axios";
+import { useFormik } from "formik";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { config } from "./config";
 
 function Product() {
   const navigate = useNavigate();
-    const [productList, setProductList] = useState([]);
+  const [productList, setProductList] = useState([]);
   const [isEdit, setEdit] = useState(false);
   const [productId, setProductId] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const products = await axios.get(`${config.api}/products`,{
-            headers : {
-                "Authorization" : localStorage.getItem("myreact")
-            }
+        const products = await axios.get(`${config.api}/products`, {
+          headers: {
+            Authorization: localStorage.getItem("myreact"),
+          },
         });
         setProductList(products.data);
       } catch (error) {
@@ -33,18 +33,19 @@ function Product() {
     onSubmit: async (values) => {
       try {
         if (!isEdit) {
-          const product = await axios.post(
-            `${config.api}/product`,
-            values,{
-              headers : {
-                  "Authorization" : localStorage.getItem("myreact")
-              }
-          }
-          );
+          const product = await axios.post(`${config.api}/product`, values, {
+            headers: {
+              Authorization: localStorage.getItem("myreact"),
+            },
+          });
           setProductList([...productList, { ...values, _id: product.data.id }]);
           formik.resetForm();
         } else {
-          await axios.put(`${config.api}/product/${productId}`, values);
+          await axios.put(`${config.api}/product/${productId}`, values, {
+            headers: {
+              Authorization: localStorage.getItem("myreact"),
+            },
+          });
           const pIndex = productList.findIndex((p) => p.id == productId);
           productList[pIndex] = values;
           setProductList([...productList]);
@@ -52,7 +53,7 @@ function Product() {
           setEdit(false);
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
         alert("Something went wrong");
       }
     },
@@ -60,19 +61,27 @@ function Product() {
 
   const deleteProduct = async (id) => {
     try {
-      await axios.delete(`${config.api}/product/${id}`);
+      await axios.delete(`${config.api}/product/${id}`, {
+        headers: {
+          Authorization: localStorage.getItem("myreact"),
+        },
+      });
       const pIndex = productList.findIndex((p) => p.id == id);
       productList.splice(pIndex, 1);
       setProductList([...productList]);
     } catch (error) {
-      console.log(error.response.data.message)
+      console.log(error.response.data.message);
       alert(error.response.data.message);
     }
   };
 
   const editProduct = async (id) => {
     try {
-      const product = await axios.get(`${config.api}/product/${id}`);
+      const product = await axios.get(`${config.api}/products/${id}`, {
+        headers: {
+          Authorization: localStorage.getItem("myreact"),
+        },
+      });
       formik.setValues(product.data);
       setProductId(id);
       setEdit(true);
@@ -82,90 +91,92 @@ function Product() {
   };
 
   const logout = () => {
-    localStorage.removeItem("myreact")
-    navigate("/")
-  }
+    localStorage.removeItem("myreact");
+    navigate("/");
+  };
   return (
     <div className="container">
-    <div className="row">
-      <div className='col-lg-12 align-right'>
-      <button className='btn btn-danger' onClick={logout}>Logout</button>
-      </div>
-      <div className="col-lg-6">
-        <form onSubmit={formik.handleSubmit}>
-          <div className="row">
-            <div className="col-lg-6">
-              <label>Name</label>
-              <input
-                type={"text"}
-                className="form-control"
-                name="name"
-                value={formik.values.name}
-                onChange={formik.handleChange}
-              />
+      <div className="row">
+        <div className="col-lg-12 align-right">
+          <button className="btn btn-danger" onClick={logout}>
+            Logout
+          </button>
+        </div>
+        <div className="col-lg-6">
+          <form onSubmit={formik.handleSubmit}>
+            <div className="row">
+              <div className="col-lg-6">
+                <label>Name</label>
+                <input
+                  type={"text"}
+                  className="form-control"
+                  name="name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                />
+              </div>
+              <div className="col-lg-6">
+                <label>Price</label>
+                <input
+                  type={"text"}
+                  className="form-control"
+                  name="price"
+                  value={formik.values.price}
+                  onChange={formik.handleChange}
+                />
+              </div>
             </div>
-            <div className="col-lg-6">
-              <label>Price</label>
-              <input
-                type={"text"}
-                className="form-control"
-                name="price"
-                value={formik.values.price}
-                onChange={formik.handleChange}
-              />
+            <div className="row mt-2">
+              <div className="col-lg-6">
+                <input
+                  type={"submit"}
+                  value={isEdit ? "Update" : "Submit"}
+                  className="btn btn-primary"
+                />
+              </div>
             </div>
-          </div>
-          <div className="row mt-2">
-            <div className="col-lg-6">
-              <input
-                type={"submit"}
-                value={isEdit ? "Update" : "Submit"}
-                className="btn btn-primary"
-              />
-            </div>
-          </div>
-        </form>
-      </div>
-      <div className="col-lg-6">
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {productList.map((product, index) => {
-              return (
-                <tr>
-                  <th scope="row">{product._id}</th>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>
-                    <button
-                      onClick={() => editProduct(product._id)}
-                      className="btn btn-info btn-sm"
-                    >
-                      Edit
-                    </button>
-                    <button
-                      onClick={() => deleteProduct(product._id)}
-                      className="btn btn-danger btn-sm"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+          </form>
+        </div>
+        <div className="col-lg-6">
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {productList.map((product, index) => {
+                return (
+                  <tr>
+                    <th scope="row">{product._id}</th>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>
+                      <button
+                        onClick={() => editProduct(product._id)}
+                        className="btn btn-info btn-sm"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => deleteProduct(product._id)}
+                        className="btn btn-danger btn-sm"
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
